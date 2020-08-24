504b001f7277890fd80e8fb5007595f4e4974767
Akshay <akshay.patel@dunelm.com>
2020-08-24 11:38:35 +0100

    Update unit test configg

---
+++ packages/api/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rootDir: process.cwd(),
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec|api))\\.jsx?$',
+  testEnvironment: 'node',
+}
---
+++ packages/app/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rootDir: process.cwd(),
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec|api))\\.jsx?$',
+  testEnvironment: 'jsdom',
+  setupFiles: [
+    '<rootDir>/setupTests.js'
+  ]
+}
